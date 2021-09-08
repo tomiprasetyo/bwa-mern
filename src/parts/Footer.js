4448699d27fb25d6d43d00fa90c9f39b60cc445f
tomiprasetyo <65531602+tomiprasetyo@users.noreply.github.com>
2021-09-08 16:27:08 +0700

    Finish slicing front end, but with some error that i don't know

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
           </div>
           <div className="col-auto mr-5">
             <h6 className="mt-2">For Beginners</h6>
-            <ul class="list-group list-group-flush">
+            <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <Button type="link" href="/register">
                   New Account
@@ -36,7 +36,7 @@ export default function Footer() {
           </div>
           <div className="col-2 mr-5">
             <h6 className="mt-2">Explore Us</h6>
-            <ul class="list-group list-group-flush">
+            <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <Button type="link" href="/career">
                   Our Career
@@ -56,7 +56,7 @@ export default function Footer() {
           </div>
           <div className="col-3">
             <h6 className="mt-2">Connect Us</h6>
-            <ul class="list-group list-group-flush">
+            <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <Button
                   isExternal
